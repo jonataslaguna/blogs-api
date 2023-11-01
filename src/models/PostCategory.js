3128b45938487fcf0b0fd5a15316f4f60c915771
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-11-01 17:45:24 -0300

    added id to the PostCategory model

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,6 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
   const PostCategory = sequelize.define(
-    'PostCategory', {},
+    'PostCategory', {
+      postId: DataTypes.INTEGER,
+      categoryId: DataTypes.INTEGER,
+    },
     {
       timestamps: false,
       tableName: 'posts_categories',
@@ -12,14 +15,14 @@ module.exports = (sequelize, DataTypes) => {
     models.BlogPost.belongsToMany(models.Category, {
       as: 'categories',
       through: PostCategory,
-      foreignKey: 'post_id', 
-      otherKey: 'category_id', 
+      foreignKey: 'postId', 
+      otherKey: 'categoryId', 
     });
     models.Category.belongsToMany(models.BlogPost, {
       as: 'posts',
       through: PostCategory,
-      foreignKey: 'category_id', 
-      otherKey: 'post_id',
+      foreignKey: 'categoryId', 
+      otherKey: 'postId',
     });
   };
 
